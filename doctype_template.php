e1dfea3246937e9b8e55a8464291d57554184f1e
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-09-30 19:14:05 +0200

    :tada: INIT project

---
+++ doctype_template.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Font link  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet">
+
+    <!-- CSS link  -->
+    <link rel="stylesheet" href="SCSS/sty">
+
+    <!-- Font Awesome link  -->
+    <script src="https://kit.fontawesome.com/c1b4de1409.js" crossorigin="anonymous"></script>
+
+    <!-- Page logo -->
+    <link rel="shortcut icon" type="image/x-icon" href="images/pdp/icon_nate.png" border-radius="50%">
+
+    <title><?php echo $pageTitle ?></title>
+</head>
