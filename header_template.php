da477cf3a117f3e0d86fa81925d68fc71e380479
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-09-30 19:19:10 +0200

    :construction: ADD header

--- header_template.php
+++ header_template.php
@@ -1,3 +1,24 @@
 <header>
-    
+    <nav> 
+        <div class="home_link">
+            <a href="#"><img src="images/pdp/logo_nate_blc.png" alt="" class="logo"></a>
+
+            <h2>
+                <a href="#" class="type_write">@jstnate</a>
+            </h2>
+        </div>
+
+        <div class="nav_link">
+            <div class="scroll_link">
+                <a href="#">About</a>
+                <a href="#">Skills</a>
+                <a href="#">Projects</a>
+            </div>
+            
+            <div class="ext_link">
+                <a href="https://github.com/jstnate"><i class="fa-brands fa-lg fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/nathanlefetey/"><i class="fa-brands fa-lg fa-linkedin"></i></a>
+            </div>
+        </div>
+    </nav>
 </header>
