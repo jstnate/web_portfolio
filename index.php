dcbe0c8c82e8e9936fc3c67def99624c2f073406
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-09-30 19:20:56 +0200

    :construction: ADD welcome section

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+    $pageTitle = "Nathan LEFETEY - Portfolio";
+    require "doctype_template.php"
+?>
+
+<body>
+    <?php require "header_template.php" ?>
+
+    <main>
+        <section class="intro_section">
+            <div>
+                <img src="images/pdp/logo_nate_grd.png" alt="">
+
+                <div class="intro_text">
+                    <h1 class="name_title">Nathan LEFETEY</h1>
+                    <h2 class="job_title">Front-end Developper</h2>
+                </div>
+            </div>
+        </section>
+    </main>
+    
+</body>
+</html>
