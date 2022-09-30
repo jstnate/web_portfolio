a636dbd8c57c614e3ce94a1b35043929bbc1dbbb
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-09-30 19:26:00 +0200

    :construction: MODIFY section name

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
     <?php require "header_template.php" ?>
 
     <main>
-        <section class="intro_section">
+        <section class="welcome_section">
             <div>
                 <img src="images/pdp/logo_nate_grd.png" alt="">
 
-                <div class="intro_text">
+                <div class="welcome_text">
                     <h1 class="name_title">Nathan LEFETEY</h1>
                     <h2 class="job_title">Front-end Developper</h2>
                 </div>
