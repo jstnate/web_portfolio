a395b2ecc36cb8710d344e6aee9c104610a33963
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-10-18 17:07:28 +0200

    :sparkle: FEAT card animation

---
+++ JS/project-card-anim.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const cards = document.querySelectorAll('.card')
+
+cards.forEach((card) => {
+    card.addEventListener("click", () => {
+        cards.forEach((card) => {
+            card.classList.remove("active")
+        })
+        card.classList.add('active')
+    })
+})
