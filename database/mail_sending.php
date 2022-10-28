c0659d371f096f815eee15c23e1ae10c8d28514e
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-10-28 16:09:29 +0200

    :lipstick: UPDATE style

--- database/mail_sending.php
+++ database/mail_sending.php
@@ -1,22 +1,25 @@
 <?php 
 
-$mail -> isSMTP();
-$mail -> Host = 'localhost';
-$mail -> Port = '1025';
+// $mail -> isSMTP();
+// $mail -> Host = 'localhost';
+// $mail -> Port = '1025';
 
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$visitor_mail = $_POST['mail'];
-$visitor_message = $_POST['message'];
+// $first_name = $_POST['first_name'];
+// $last_name = $_POST['last_name'];
+// $visitor_mail = $_POST['email'];
+// $visitor_message = $_POST['message'];
 
-$to = "lefetey.nathan@gmail.com";
-$email_subject = "Contact from portfolio";
-$email_body = $visitor_message;
-$email_from = $visitor_mail;
-$headers = "From: $email_from";
+// $to = "lefetey.nathan@gmail.com";
+// $email_subject = "Contact request from portfolio";
+// $email_body = $visitor_message;
+// $email_from = $visitor_mail;
+// $headers = "From : $email_from";
 
-mail($to, $email_subject, $first_name, $last_name, $email_body, $headers);
-
-header('Location: test.php');
-
+// mail($to, $email_subject, $email_body, $headers);
+// var_dump($to);
+// var_dump($email_subject);
+// var_dump($email_body);
+// var_dump($headers);
+// $submitted = true;
+// header('Location: ../index.html#form');
 ?>
