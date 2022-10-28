d938476517983e30d6d6dd66dfaf202f30b82159
Nathan Lefetey <lefetey.nathan@gmail.com>
2022-10-28 17:47:07 +0200

    :sparkles: FEAT blur effect anim

--- JS/smooth-blur-anim.js
+++ JS/smooth-blur-anim.js
@@ -1,38 +1,26 @@
 'use strict'
 
-const animRatio = 0.5
+const animRatio = 0.3
 const animOptions = {
     root: null,
     rootMargin: '50px',
     threshold: animRatio
 }
 
-const animatedSection = document.querySelectorAll(".smooth_appear")
-
-
-// if (animatedSection != null) {
-//     const observerForAnim = function (revealEntries) {
-//         revealEntries.forEach((entry) => {
-//             if(entry.intersectionRatio > animRatio){
-//                 entry.target.classList.add("blur_reveal")
-//             }
-//         })
-//     }
-    
-//     const anim = new IntersectionObserver(observerForAnim, animOptions)
-//     anim.observe(animatedSection)    
-// }
-
-if (animatedSection != null) {
-    const animObserver = function (blurEntries) {
-        blurEntries.forEach((blurEntry) => {
-            blurEntry.target.classList.add('blur_reveal')
-        })
+const animatedSection = document.querySelectorAll('.smooth_appear').forEach((item) => {
+    if (item != null) {
+        const animObserver = function (revealEntries_1, reveal_1) {
+            revealEntries_1.forEach(function(entry_1) {
+                if(entry_1.intersectionRatio > animRatio){
+                    reveal_1.unobserve(entry_1.target)
+                    entry_1.target.classList.add("blur_reveal")
+                }
+            });
+        }
         
         const animated = new IntersectionObserver(animObserver, animOptions)
-
-        animatedSection.forEach((item) => {
-            animated.observe(item)
-        })
+        animated.observe(item)
     }
-}
+    
+})
+
